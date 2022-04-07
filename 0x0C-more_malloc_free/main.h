2b1880eca68b83612dfe9504432dacfc1fcb0cb4
Adekomi Abdulbaqi <adekomiabdulbaqi3@gmail.com>
2022-04-07 22:56:26 +0000

    done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -8,6 +11,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 
